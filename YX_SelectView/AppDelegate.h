22fc712811a0efbf464244d5a894429263529087
populary <410445919@qq.com>
2016-08-05 09:24:28 +0800

    Initial Commit

---
+++ YX_SelectView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YX_SelectView
+//
+//  Created by yang on 16/8/5.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YX_SelectView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YX_SelectView
+//
+//  Created by yang on 16/8/5.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
